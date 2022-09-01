0a685c79ae0bc80b7c0936faf83cddceac459300
Leonardo Cisternas <leocisternasa@gmail.com>
2022-09-01 13:18:55 -0400

    Portfolio update, components navbar, about, Main and Skills

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
+import Main from '../components/Main'
+import About from '../components/About'
+import Skills from '../components/Skills'
 
 
 export default function Home() {
@@ -12,6 +15,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
+      <Main />
+      <About />
+      <Skills />
+
 
 
     </div>
