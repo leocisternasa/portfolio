64fe62f52262deb28634eb47a6c0d6c9b9a0304a
Leonardo Cisternas <leocisternasa@gmail.com>
2022-09-01 22:18:08 -0400

    Front finished, Adding function to contact message by email with SendGrid

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 import { AiOutlineMail } from 'react-icons/ai'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
@@ -5,7 +6,7 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 
 function Main() {
   return (
-    <div className='w-full h-screen text-center'>
+    <div id='home' className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           <p className='uppercase text-sm tracking-widest text-gray-600'>LET'S BUILD SOMETHING TOGETHER</p>
@@ -13,12 +14,20 @@ function Main() {
           <h1 className='py-4 text-gray-700'>A Front-End Web Developer</h1>
           <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'm a front-end web developer specializing in building exceptional digital experiences.Curently, I'm focusing on improving my fron-end skills while learning back-end technologies</p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
-            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
-              <FaLinkedinIn />
-            </div>
-            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
-              <FaGithub />
-            </div>
+
+            <a href='https://www.linkedin.com/in/leonardo-cisternas-674813249/' target='_blank' rel="noopener noreferrer">
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <FaLinkedinIn />
+
+              </div>
+            </a>
+
+            <a href='https://github.com/leocisternasa' target='_blank' rel="noopener noreferrer">
+
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <FaGithub />
+              </div>
+            </a>
             <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
               <AiOutlineMail />
             </div>
