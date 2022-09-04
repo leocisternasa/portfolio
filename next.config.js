674663aba0a73f9b45c77fc3f036aee152e2dd8a
Leonardo Cisternas <leocisternasa@gmail.com>
2022-09-04 10:56:15 -0400

    deployed with firebase without image optimization

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  
+
+}
+
+module.exports = module.exports = {
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+}
